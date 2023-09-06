0f3ba639de9ef894709f62fe6acbb0c53c57b941
hesamsaeidi <66099111+hesamsaeidi@users.noreply.github.com>
2023-09-06 16:27:06 -0500

    call_taup updated and relative trim added to trim the data relative to P arrival

--- nec_func.py
+++ nec_func.py
@@ -14,7 +14,16 @@ def log_to_file(fileName,logContent):
         
 def call_taup(stream, model, phase_list):
     model = TauPyModel(model=model)
-    
-    arrivals = model.get_travel_times(source_depth_in_km=100,
-                                  distance_in_degree=45,
+    source_depth = stream[0].stats.sac['evdp']/1000 #convert to km
+    distance = stream[0].stats.sac['gcarc']
+    arrivals = model.get_travel_times(source_depth_in_km=source_depth,
+                                  distance_in_degree=distance,
                                   phase_list=phase_list)
+    return arrivals
+   
+   
+def relative_trim(stream, arrivals, a, b):
+    # trim the input stream with relative times.
+    p_arrival = stream[0].stats.starttime + stream[0].stats.sac['o'] + arrivals[0].time
+    stream.trim(p_arrival+a, p_arrival+b)
+    return stream
